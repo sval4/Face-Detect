0744396b474cf9913544c5f26cf01e4361554624
sval4 <val.sharat@gmail.com>
2023-09-09 16:55:20 -0400

    Centered displayed emotion/Comments

--- facedetect.py
+++ facedetect.py
@@ -15,8 +15,10 @@ while True:
     face = cascadeFace.detectMultiScale(grayscale, 1.3, 5)
 
     for (x, y, w, h) in face:
+        #Creates frame around face
         cv2.rectangle(img, (x,y), (x + w, y + h), (255, 0, 0), 2)
 
+        #Selects portion of image that is only the face
         faceROI = img[y:y + h, x:x + w]
 
         emotion = emotionDetector.detect_emotions(faceROI)
@@ -24,7 +26,13 @@ while True:
         if emotion:
             #print(emotion[0]["emotions"])
             mainEmotion = max(emotion[0]["emotions"], key=lambda x: emotion[0]["emotions"][x])
-            cv2.putText(img, mainEmotion, (x,y - 10), cv2.FONT_HERSHEY_COMPLEX, 0.9, (255, 255, 255), 2)
+
+            #Centers emotion display
+            wordLength = len(mainEmotion)
+            print(wordLength)
+            x_pos = x + (w // 2) - (wordLength * 9)
+
+            cv2.putText(img, mainEmotion.title(), (x_pos, y - 10), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
 
     cv2.imshow("img", img)
     wait = cv2.waitKey(50)
